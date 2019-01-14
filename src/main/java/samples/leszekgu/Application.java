bbabf2b9dab25022e13e96468506a32f171fc6ca
Leszek Gurniewicz <l.gurniewicz@kainos.com>
2019-01-14 17:18:21 +0100

    Commandline version

--- src/main/java/samples/leszekgu/Application.java
+++ src/main/java/samples/leszekgu/Application.java
@@ -1,13 +1,36 @@
 package samples.leszekgu;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import samples.leszekgu.jdbc.JdbcSampleService;
+import samples.leszekgu.jpa.JpaService;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
     public static void main(String args[]) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Autowired
+    private JdbcSampleService jdbc;
+
+    @Autowired
+    private JpaService jpa;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        jdbc.run();
+        try {
+            jpa.run();
+        } catch (ObjectOptimisticLockingFailureException ex) {
+            System.out.println("\t\t\tOptLoc");
+            ex.printStackTrace(System.out);
+        }
+
+    }
 }
